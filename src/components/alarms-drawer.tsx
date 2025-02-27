871368b6f1dce31c2081c84e1e8f221350a3b69b
Harun29 <ibrahimovic.harun01@gmail.com>
2025-02-27 13:14:22 +0100

    Refactor AlarmsDrawer component to enhance alarm data structure; add severity levels and dynamic styling; implement scrollable container for alarms display

--- src/components/alarms-drawer.tsx
+++ src/components/alarms-drawer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { BellRing } from "lucide-react";
+import { BellRing, Info, TriangleAlert } from "lucide-react";
 import { useState } from "react";
 import { Button } from "./ui/button";
 import {
@@ -18,22 +18,39 @@ import {
   DrawerDescription,
   DrawerHeader,
   DrawerTitle,
-  DrawerTrigger
+  DrawerTrigger,
 } from "./ui/drawer";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { Progress } from "./ui/progress";
 
 interface Alarm {
   id: number;
-  device: string;
-  message: string;
+  name: string;
+  value: number;
+  severity: "low" | "medium" | "high";
 }
 
 const AlarmsDrawer = () => {
   const [alarms, setAlarms] = useState<Alarm[]>([
-    { id: 1, device: "Trash Can", message: "65% full" },
-    { id: 2, device: "Light2", message: "Failed" },
-    { id: 3, device: "Bench1", message: "Occupied 2/3" },
-    { id: 4, device: "Bench1", message: "Occupied 2/3" },
+    { id: 1, name: "Trash Can", value: 65, severity: "low" },
+    { id: 2, name: "Light2", value: 0, severity: "medium" },
+    { id: 3, name: "Bench1", value: 33, severity: "high" },
+    { id: 4, name: "Light2", value: 0, severity: "medium" },
+    { id: 5, name: "Light2", value: 0, severity: "medium" },
+    { id: 6, name: "Bench1", value: 33, severity: "high" },
+    { id: 7, name: "Bench1", value: 33, severity: "high" },
+    { id: 8, name: "Light2", value: 0, severity: "medium" },
+    { id: 9, name: "Bench1", value: 33, severity: "high" },
+    { id: 10, name: "Bench1", value: 33, severity: "high" },
+    { id: 11, name: "Trash Can", value: 65, severity: "low" },
+    { id: 12, name: "Bench1", value: 33, severity: "high" },
+    { id: 13, name: "Bench1", value: 66, severity: "low" },
   ]);
 
   const assignToWorker = (alarmId: number) => {
@@ -57,49 +74,71 @@ const AlarmsDrawer = () => {
             Click on an alarm to assign it to a user.
           </DrawerDescription>
         </DrawerHeader>
-        <div className="p-4">
-          {alarms.map((alarm) => (
-            <div
-              key={alarm.id}
-              className="bg-gray-100 p-4 mb-3 rounded-lg flex justify-between items-center"
-            >
-              <div>
-                <strong>{alarm.device}</strong>: {alarm.message}
+
+        {/* Scrollable container */}
+        <div className=" overflow-x-auto custom-scrollbar m-6">
+          <div className="flex space-x-3 min-w-max">
+            {alarms.map((alarm) => (
+              <div
+                key={alarm.id}
+                className="bg-gray-100 rounded-lg overflow-hidden w-44 flex-none flex flex-col justify-between items-center h-64 mb-4"
+              >
+                <div
+                  className={`p-4 flex flex-col items-center gap-2 w-full h-4/5 text-white ${
+                    alarm.severity === "low"
+                      ? "bg-blue-300"
+                      : alarm.severity === "medium"
+                      ? "bg-yellow-300"
+                      : "bg-red-300"
+                  }`}
+                >
+                  <div className="flex flex-col items-center gap-2">
+                    {alarm.severity === "low" && <Info className="h-16 w-16" />}
+                    {alarm.severity === "medium" && <TriangleAlert className="h-16 w-16" />}
+                    {alarm.severity === "high" && <TriangleAlert className="h-16 w-16" />}
+                    <span>{alarm.name}</span>
+                    <span>{alarm.value}% full</span>
+                  </div>
+                  <div className="w-full">
+                    <Progress className="text-white" value={alarm.value} />
+                  </div>
+                </div>
+
+                <Dialog>
+                  <DialogTrigger disabled={alarm.severity === "low"} className="h-1/5 w-full" asChild>
+                    <Button
+                      variant="ghost"
+                      onClick={() => assignToWorker(alarm.id)}
+                    >
+                      Assign
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Assign this to a worker!</DialogTitle>
+                      <DialogDescription>
+                        Pick a worker this will be assigned to. They will be notified immediately.
+                      </DialogDescription>
+                      <Select>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a worker" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem value="1">John Doe</SelectItem>
+                          <SelectItem value="2">Jane Doe</SelectItem>
+                          <SelectItem value="3">Alice</SelectItem>
+                          <SelectItem value="4">Bob</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </DialogHeader>
+                    <DialogFooter>
+                      <Button>Assign</Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
               </div>
-              <Dialog>
-                <DialogTrigger asChild>
-                  <Button
-                    variant="ghost"
-                    onClick={() => assignToWorker(alarm.id)}
-                  >
-                    Assign
-                  </Button>
-                </DialogTrigger>
-                <DialogContent>
-                  <DialogHeader>
-                    <DialogTitle>Assign this to a worker!</DialogTitle>
-                    <DialogDescription>
-                      Pick a worker this will be assigned to. He will be notified immediately.
-                    </DialogDescription>
-                    <Select>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select a worker" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem value="1">John Doe</SelectItem>
-                        <SelectItem value="2">Jane Doe</SelectItem>
-                        <SelectItem value="3">Alice</SelectItem>
-                        <SelectItem value="4">Bob</SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </DialogHeader>
-                  <DialogFooter>
-                    <Button>Assign</Button>
-                  </DialogFooter>
-                </DialogContent>
-              </Dialog>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </DrawerContent>
     </Drawer>
